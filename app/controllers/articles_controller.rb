ce2804ad6a123ab257abd44ffb0b34f434f248f7
Chris Johnston <cjohnston@connected.io>
2021-01-19 16:50:16 -0500

    adds basic css

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,27 @@
 class ArticlesController < ApplicationController
-    def index
-        
-    end
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    Article.find(params[:id]).destroy
+  end
 end
