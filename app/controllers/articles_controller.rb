23873b61fdb0e42aa150dceee89ce11b0148c631
Christopher Johnston <cjohnston@connected.io>
2021-02-17 11:34:51 -0500

    Adds images and markdown rendered text to show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,7 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
   end
 
   def new
